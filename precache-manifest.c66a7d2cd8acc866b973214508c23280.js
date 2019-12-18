0f07faa21f56956c7e2f680b488d571af7e6b89c
Dylan Pereira <perdylan@gmail.com>
2019-12-18 16:47:51 +1100

    Updates

---
+++ precache-manifest.c66a7d2cd8acc866b973214508c23280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee3e2d11e84d528c4f9d4899b2e81d5",
+    "url": "/the_game_show/index.html"
+  },
+  {
+    "revision": "361404514ce7133b9b53",
+    "url": "/the_game_show/static/css/main.383c521c.chunk.css"
+  },
+  {
+    "revision": "19453bc8b2764de7ee0b",
+    "url": "/the_game_show/static/js/2.8b9adb8f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/the_game_show/static/js/2.8b9adb8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "361404514ce7133b9b53",
+    "url": "/the_game_show/static/js/main.4ce2f9b0.chunk.js"
+  },
+  {
+    "revision": "64c90ee1777b44b49c98",
+    "url": "/the_game_show/static/js/runtime-main.8489a557.js"
+  }
+]);
